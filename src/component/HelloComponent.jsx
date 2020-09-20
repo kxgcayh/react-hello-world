95f4333350c58b1386881f9fa615cdaad30c54c8
Kautsar Al Bana <kautsaralbana@hotmail.com>
2020-09-21 01:48:35 +0700

    ReactJs 101

---
+++ src/component/HelloComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HelloComponent.css";
+
+// Contoh Stateless Component
+const HelloComponent = () => {
+  return (
+    <p className="text-p">Text ini dibuat menggunakan StateLess Component</p>
+  );
+};
+
+export default HelloComponent;
---
+++ src/container/StateFullComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Contoh StateFull Component
+class StateFullComponent extends React.Component {
+  render() {
+    return <p>Text ini dibuat menggunakan StateFull Component</p>;
+  }
+}
+
+export default StateFullComponent;
