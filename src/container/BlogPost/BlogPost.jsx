5d057a6e2f84a2911d2b99aaea4c150c704ee9b1
Kautsar Al Bana <kautsaralbana@hotmail.com>
2020-09-22 11:23:58 +0700

    handleFormChange

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,10 +1,17 @@
 import React, { Component, Fragment } from "react";
 import Post from "../../component/Post/Post";
 import axios from "axios";
+import { Form, Card, Button } from "react-bootstrap";
 
 class BlogPost extends Component {
   state = {
     post: [],
+    formBlogPost: {
+      userId: 1,
+      id: 1,
+      title: "",
+      body: "",
+    },
   };
 
   getPostAPI = () => {
@@ -21,6 +28,20 @@ class BlogPost extends Component {
     });
   };
 
+  handleFormChange = (event) => {
+    let formBlogPostNew = { ...this.state.formBlogPost };
+    console.log("Terjadi perubahan di: ", event.target.name);
+    formBlogPostNew[event.target.name] = event.target.value;
+    this.setState(
+      {
+        formBlogPost: formBlogPostNew,
+      },
+      () => {
+        console.log("Value Obj formBlogPost ", this.state.formBlogPost);
+      }
+    );
+  };
+
   componentDidMount() {
     // Get API from Method
     this.getPostAPI();
@@ -29,9 +50,44 @@ class BlogPost extends Component {
   render() {
     return (
       <Fragment>
-        {this.state.post.map((post) => {
-          return <Post key={post.id} data={post} remove={this.handleRemove} />;
-        })}
+        <Card>
+          <Card.Body>
+            <Form>
+              <Form.Group>
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                  name="title"
+                  type="text"
+                  placeholder="Enter Title"
+                  onChange={this.handleFormChange}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Blog Content</Form.Label>
+                <Form.Control
+                  name="body"
+                  as="textarea"
+                  placeholder="Enter Content"
+                  onChange={this.handleFormChange}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Button variant="primary">Submit</Button>
+              </Form.Group>
+              <Form.Group>
+                {this.state.post.map((post) => {
+                  return (
+                    <Post
+                      key={post.id}
+                      data={post}
+                      remove={this.handleRemove}
+                    />
+                  );
+                })}
+              </Form.Group>
+            </Form>
+          </Card.Body>
+        </Card>
       </Fragment>
     );
   }
